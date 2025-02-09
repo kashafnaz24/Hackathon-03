58b8ee26dc3e736115f8fdfb41eff23a193f228a
Kashaf Naz <kashafnazk389@gmail.com>
2025-02-09 20:13:38 +0500

    hackathon3 next.js with sanity

---
+++ hackaton3/src/app/singleProduct/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { client } from "@/sanity/lib/client";
+import ProductDetailClient from "@/app/component/productDetail";
+
+const ProductDetailPage = async ({ params }: { params: { id: string } }) => {
+  async function getProduct(id: string) {
+    try {
+      return await client.fetch(`*[_type == "products" && _id == $id][0]`, { id });
+    } catch (error) {
+      console.error("Error fetching product:", error);
+      return null;
+    }
+  }
+
+  try {
+    const product = await getProduct(params.id);
+
+    if (!product) {
+      return <p className="text-center text-red-500">Product not found!</p>;
+    }
+
+    return <ProductDetailClient product={product} />;
+  } catch (error) {
+    console.error("Error rendering product detail page:", error);
+    return <p className="text-center text-red-500">Something went wrong!</p>;
+  }
+};
+
+export default ProductDetailPage;
+
